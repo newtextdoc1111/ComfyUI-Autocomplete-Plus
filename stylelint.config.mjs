a9ebdec3fa05ed5dbacf6eb74538366fca9f5f2e
newtextdoc1111 <newtextdoc1111@gmail.com>
2025-05-04 12:40:33 +0900

    chore: add package.json and stylelint configuration
    - Created package.json to manage development dependencies for stylelint.
    - Added stylelint and related configurations in stylelint.config.mjs.

---
+++ stylelint.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('stylelint').Config} */
+export default {
+    "extends": [
+        "stylelint-config-standard",
+        "stylelint-config-idiomatic-order"
+    ],
+    "plugins": [
+        "stylelint-order"
+    ]
+}
