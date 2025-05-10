029d3a5a763401ddac2cad01ce220cb664bfc4a7
newtextdoc1111 <newtextdoc1111@gmail.com>
2025-05-10 16:08:09 +0900

    Remove unnecessary comment

--- __init__.py
+++ __init__.py
@@ -4,7 +4,6 @@ from .modules import downloader
 # check and download necessary csv files
 downloader.check_and_download_csv_files()
 
-# --- Original Node Mappings (Keep or modify as needed) ---
 WEB_DIRECTORY = "./web"
 NODE_CLASS_MAPPINGS = {}
 __all__ = []
